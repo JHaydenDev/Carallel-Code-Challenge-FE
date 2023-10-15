66cc447ae9ae2946df43cecdb5bfac8b218b7b45
joseph hayden <josephhaydendevelopment@gmail.com>
2023-10-15 18:16:18 -0500

    .

---
+++ my-app/src/app/GatedArticles/page.tsx
@@ -0,0 +1,19 @@
+// import { Article } from "@/components/Article";
+
+export default function Page() {
+  return (
+    <>
+      <div>
+        asdf
+        {/* {articles?.map((article: any) => (
+          <Article
+            key={article.id}
+            id={article.id}
+            title={article.title}
+            text={article.text}
+          />
+        ))} */}
+      </div>
+    </>
+  );
+}
